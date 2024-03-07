bacd89d11ca6a5905e32624a96e685018afee543
Cai Chun Yan <caichunyan17@gmail.com>
2024-03-07 14:27:57 -0800

    Hello world!

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ca.bcit.comp2522.termproject.comp2522202410termprojecteg1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ca.bcit.comp2522.termproject.comp2522202410termprojecteg1 to javafx.fxml;
+    exports ca.bcit.comp2522.termproject.comp2522202410termprojecteg1;
+}
