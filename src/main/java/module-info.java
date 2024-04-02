17dd37720bbbeeb776487d832607f9cd1b2243a6
Cai Chun Yan <caichunyan17@gmail.com>
2024-04-02 14:00:41 -0700

    initialize LibGDX

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ca.bcit.comp2522.termproject.comp2522202410termprojecteg1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ca.bcit.comp2522.termproject.comp2522202410termprojecteg1 to javafx.fxml;
-    exports ca.bcit.comp2522.termproject.comp2522202410termprojecteg1;
-}
