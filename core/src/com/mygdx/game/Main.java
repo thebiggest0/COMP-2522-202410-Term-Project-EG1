9e895883fd6e65ec32a19073a4cf1347b3045156
Yifei Zeng <yzeng33@my.bcit.ca>
2024-04-03 13:25:56 -0700

    Implement create method

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -24,4 +26,28 @@ public class Main extends ApplicationAdapter {
 	float speedTieFighter = 100;
 
 	Vector2 offsetTieFighter;
+
+	@Override
+	public void create () {
+		offsetTieFighter = Vector2.Zero;
+		batch = new SpriteBatch();
+		imagePlayer = new Texture("player.png");
+		imageLaser = new Texture("bullet.png");
+		imageTieFighter = new Texture("alien.png");
+		player = new Player(imagePlayer, imageLaser);
+		tieFighter = new TieFighter[widthCount * heightCount];
+		int i = 0;
+		for (int y = 0; y < heightCount; y++) {
+			for (int x = 0; x < widthCount; x++) {
+				Vector2 position = new Vector2(x * spacingTieFighter, y * spacingTieFighter);
+				position.x += (float) Gdx.graphics.getWidth() / 2;
+				position.y += Gdx.graphics.getHeight();
+				position.x -= ((float) widthCount / 2) * spacingTieFighter;
+				position.y -= (heightCount) * spacingTieFighter;
+
+				tieFighter[i] = new TieFighter(position, imageTieFighter, Color.GREEN);
+				i++;
+			}
+		}
+	}
 }
