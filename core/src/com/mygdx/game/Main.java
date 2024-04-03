752410165200417e74b5d6ab972ebf9f6a49c789
Yifei Zeng <yzeng33@my.bcit.ca>
2024-04-03 13:53:16 -0700

    Implement render method

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -18,6 +18,7 @@ public class Main extends ApplicationAdapter {
 	int widthCount = 4;
 	int heightCount = 3;
 	int spacingTieFighter = 50;
+	int amountTieFighterAlive = 0;
 	int minXTieFighter;
 	int minYTieFighter;
 	int maxXTieFighter;
@@ -50,4 +51,89 @@ public class Main extends ApplicationAdapter {
 			}
 		}
 	}
+
+	@Override
+	public void render () {
+		float deltaTime = Gdx.graphics.getDeltaTime();
+		ScreenUtils.clear(0, 0, 0, 1);
+		batch.begin();
+		player.Draw(batch);
+
+
+		int alien_length = tieFighter.length;
+        for (TieFighter fighter : tieFighter) {
+            if (fighter.Alive) {
+                if (player.spriteBullet.getBoundingRectangle().overlaps(fighter.sprite.getBoundingRectangle())) {
+                    player.positionBullet.y = 1000;
+                    fighter.Alive = false;
+                    break;
+                }
+            }
+        }
+
+
+		minXTieFighter = 10000;
+		minYTieFighter = 10000;
+		maxXTieFighter = 0;
+		maxYTieFighter = 0;
+
+		amountTieFighterAlive = 0;
+		for (int i = 0; i < alien_length; i++) {
+			if (tieFighter[i].Alive) {
+				int IndexX = i % widthCount;
+				int IndexY = i / widthCount;
+
+				if (IndexX > maxXTieFighter) {
+					maxXTieFighter = IndexX;
+				}
+				if (IndexX < minXTieFighter) {
+					minXTieFighter = IndexX;
+				}
+				if (IndexY > maxYTieFighter) {
+					maxYTieFighter = IndexY;
+				}
+				if (IndexY < minYTieFighter) {
+					minYTieFighter = IndexY;
+				}
+				amountTieFighterAlive++;
+			}
+		}
+
+		if (amountTieFighterAlive == 0) {
+			speedTieFighter += 20;
+			for (int i = 0; i < alien_length; i++) {
+				tieFighter[i].Alive = true;
+			}
+			offsetTieFighter = new Vector2(0,0);
+			batch.end();
+			return;
+		}
+
+		offsetTieFighter.x += directionTieFighter * deltaTime * speedTieFighter;
+		if (tieFighter[maxXTieFighter].position.x >= Gdx.graphics.getWidth()) {
+			directionTieFighter = -1;
+
+			offsetTieFighter.y -= tieFighter[0].sprite.getHeight() * tieFighter[0].sprite.getScaleY() * 0.25f;
+		}
+		if (tieFighter[minXTieFighter].position.x <= 0) {
+			directionTieFighter = 1;
+			offsetTieFighter.y -= tieFighter[0].sprite.getHeight() * tieFighter[0].sprite.getScaleY() * 0.25f;
+		}
+
+		if (tieFighter[minYTieFighter].position.y <= 0) {
+			Gdx.app.exit();
+		}
+
+		for (int i = 0; i < alien_length; i++) {
+			tieFighter[i].position = new Vector2 (tieFighter[i].position_initial.x + offsetTieFighter.x, tieFighter[i].position_initial.y + offsetTieFighter.y);
+			if (tieFighter[i].Alive) {
+				tieFighter[i].Draw(batch);
+
+				if (tieFighter[i].sprite.getBoundingRectangle().overlaps(player.sprite.getBoundingRectangle())) {
+					Gdx.app.exit();
+				}
+			}
+		}
+		batch.end();
+    }
 }
