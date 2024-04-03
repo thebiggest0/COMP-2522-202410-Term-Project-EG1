1f6803ea00c097e2e12d646b4d1cee43052d5ecb
Cai Chun Yan <caichunyan17@gmail.com>
2024-04-02 19:14:52 -0700

    Add laser feature to Player class

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -9,16 +9,27 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Player {
     public Vector2 position;
+    public Vector2 positionLaser;
     public Sprite sprite;
+    public Sprite spriteLaser;
     public float speed = 500;
+    public float speedBullet = 1000;
 
-    public Player(Texture image) {
+    public Player(Texture image, Texture imageLaser) {
         sprite = new Sprite(image);
+        spriteLaser = new Sprite(imageLaser);
         sprite.setScale(4);
+        spriteLaser.setScale(4);
         position = new Vector2(Gdx.graphics.getWidth()/2,sprite.getScaleY()*sprite.getHeight()/2);
+        positionLaser = new Vector2(0, 1000);
     }
 
     public void update (int deltaTime) {
+        // bullet clicking
+        if (Gdx.input.isButtonJustPressed(0) && positionLaser.y >= Gdx.graphics.getHeight()) {
+            positionLaser.x = position.x + 5;
+            positionLaser.y = 0;
+        }
         // player movement speed
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             position.x -= deltaTime*speed;
@@ -34,6 +45,7 @@ public class Player {
         if (position.x + (sprite.getWidth() * sprite.getScaleX()/2) >= Gdx.graphics.getWidth()) {
             position.x = Gdx.graphics.getWidth() - sprite.getWidth() * sprite.getScaleX()/2;
         }
+        positionLaser.y += deltaTime * speedBullet;
     }
 
     public void Draw(SpriteBatch batch) {
