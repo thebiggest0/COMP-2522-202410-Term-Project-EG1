5e6eae95138b7bfbe074efccf49e4555ae128f6b
Yifei Zeng <yzeng33@my.bcit.ca>
2024-04-03 13:13:39 -0700

    Modify Player class

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -9,28 +9,26 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Player {
     public Vector2 position;
-    public Vector2 positionLaser;
+    public Vector2 positionBullet;
     public Sprite sprite;
-    public Sprite spriteLaser;
+    public Sprite spriteBullet;
     public float speed = 500;
     public float speedBullet = 1000;
-
-    public Player(Texture image, Texture imageLaser) {
+    public Player(Texture image, Texture imageBullet) {
         sprite = new Sprite(image);
-        spriteLaser = new Sprite(imageLaser);
+        spriteBullet = new Sprite(imageBullet);
         sprite.setScale(4);
-        spriteLaser.setScale(4);
-        position = new Vector2(Gdx.graphics.getWidth()/2,sprite.getScaleY()*sprite.getHeight()/2);
-        positionLaser = new Vector2(0, 1000);
+        spriteBullet.setScale(4);
+        position = new Vector2((float) Gdx.graphics.getWidth() /2,sprite.getScaleY()*sprite.getHeight()/2);
+        positionBullet = new Vector2(0, 1000);
     }
 
-    public void update (int deltaTime) {
-        // bullet clicking
-        if (Gdx.input.isButtonJustPressed(0) && positionLaser.y >= Gdx.graphics.getHeight()) {
-            positionLaser.x = position.x + 5;
-            positionLaser.y = 0;
+    public void Update(float deltaTime) {
+        if (Gdx.input.isButtonJustPressed(0) && positionBullet.y >= Gdx.graphics.getHeight()) {
+            positionBullet.x = position.x + 5;
+            positionBullet.y = 0;
         }
-        // player movement speed
+
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             position.x -= deltaTime*speed;
         }
@@ -38,18 +36,21 @@ public class Player {
             position.x += deltaTime*speed;
         }
 
-        // player stop move at wall
         if (position.x - (sprite.getWidth() * sprite.getScaleX()/2) <= 0) {
             position.x = sprite.getWidth() * sprite.getScaleX()/2;
         }
         if (position.x + (sprite.getWidth() * sprite.getScaleX()/2) >= Gdx.graphics.getWidth()) {
             position.x = Gdx.graphics.getWidth() - sprite.getWidth() * sprite.getScaleX()/2;
         }
-        positionLaser.y += deltaTime * speedBullet;
+
+        positionBullet.y += deltaTime * speedBullet;
     }
 
     public void Draw(SpriteBatch batch) {
+        Update(Gdx.graphics.getDeltaTime());
         sprite.setPosition(position.x, position.y);
         sprite.draw(batch);
+        spriteBullet.setPosition(positionBullet.x, positionBullet.y);
+        spriteBullet.draw(batch);
     }
 }
