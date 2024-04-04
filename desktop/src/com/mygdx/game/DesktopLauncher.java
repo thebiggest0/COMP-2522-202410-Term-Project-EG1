d23a3468e75972f87409b62cace76f53dd36b890
Cai Chun Yan <caichunyan17@gmail.com>
2024-04-04 13:53:17 -0700

    Update game name in DesktopLauncher

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Test");
+		config.setTitle("Star Wars Game");
 		new Lwjgl3Application(new Main(), config);
 	}
 }
