ca70b25772b25dce0ff2f0267e722a6a6bf270ef
Cai Chun Yan <caichunyan17@gmail.com>
2024-04-06 23:55:24 -0700

    Add code for a boss enemy

--- core/src/com/mygdx/game/Boss.java
+++ core/src/com/mygdx/game/Boss.java
@@ -1,11 +1,52 @@
 package com.mygdx.game;
 
-/**
- * Represents Boss.
- *
- * @author Yifei, Steven
- * @version 2024
- */
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 public class Boss {
+    public Vector2 position;
+    private Texture texture;
+    private int hp;
+    private Rectangle boundingBox;
+    private int scale = 20; // Adjust this value to scale the boss size
+    private float speed = 50;
+
+    public Boss(Vector2 position) {
+//        this.position = position;
+        this.texture = new Texture("alien.png"); // Assume you have a boss texture
+        this.position = new Vector2(Gdx.graphics.getWidth() / 2f - texture.getWidth() / 2f, Gdx.graphics.getHeight());
+        this.hp = 15;
+        this.boundingBox = new Rectangle(position.x, position.y, texture.getWidth() * scale, texture.getHeight() * scale);
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(texture, position.x, position.y, texture.getWidth() * scale, texture.getHeight() * scale);
+    }
+
+    public int getHp() {
+        return this.hp;
+    }
+
+    public void hit() {
+        hp -= 1;
+    }
+
+    public Rectangle getBoundingBox() {
+        return boundingBox;
+    }
+
+    // Add methods to update the boss's position and bounding box if needed
+    public void update(float deltaTime) {
+        position.y -= speed * deltaTime;
+        updateBoundingBox();
+    }
+
+    private void updateBoundingBox() {
+        boundingBox.setPosition(position.x, position.y);
+    }
+
 }
